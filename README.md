5700101b5062e5cd01e8509734c5098ab1b0810f
indralukmana <indralukmana@users.noreply.github.com>
2022-10-10 19:30:23 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Base-Vite-Tailwind
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-x5tew4)
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function App() {
+  const [] = React.useState([]);
+
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
